48a7ee7ae7f54be3fd1a36f67d1d2d6c4cc6240b
Michal Kunert <mkunert@virtuslab.com>
2025-05-09 20:53:08 +0200

    fix: remove unused var

--- src/app/app/profile/actions.ts
+++ src/app/app/profile/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
-import { profileSchema, ProfileValues } from "./types";
+import { profileSchema } from "./types";
 
 export async function getProfile() {
   const supabase = await createClient();
